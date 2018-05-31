d6cf57e46fa24d6f12c140dda1d39259bb2aebeb
Hubert Zhang <hzhang@pivotal.io>
2018-05-31 05:13:18 -0400

    d

--- concourse/scripts/compile_gpdb_open_source_centos.bash
+++ concourse/scripts/compile_gpdb_open_source_centos.bash
@@ -8,6 +8,7 @@ function prep_env_for_centos() {
   export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.39.x86_64
   export PATH=${JAVA_HOME}/bin:${PATH}
   install_system_deps
+  yum instlal -y python36-devel
 }
 
 function install_system_deps() {
@@ -54,6 +55,16 @@ function build_gpdb() {
     # a single-CPU system
     make -j4 -s
     make -s install
+
+    PYTHON=/usr/bin/python3.6 CC=$(which gcc) CXX=$(which g++) ./configure --enable-mapreduce --with-perl --with-libxml \
+	--disable-orca --with-python --disable-gpfdist --prefix=${GREENPLUM_INSTALL_DIR}
+    # Use -j4 to speed up the build. (Doesn't seem worth trying to guess a better
+    # value based on number of CPUs or anything like that. Going above -j4 wouldn't
+    # make it much faster, and -j4 is small enough to not hurt too badly even on
+    # a single-CPU system
+    make -j4 -s
+    make -s install
+
   popd
 }
 
