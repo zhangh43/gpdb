b3b10b484e5434542c2a1754efb833224027e68f
Hubert Zhang <hzhang@pivotal.io>
2018-06-05 08:28:46 -0400

    change name

--- src/pl/plpython/sql/plpython3.sql
+++ src/pl/plpython/sql/plpython3.sql
@@ -1,21 +1,21 @@
 create language plpython3u;
 
-CREATE FUNCTION import_succeed() returns text AS $$
+CREATE FUNCTION import_succeed3() returns text AS $$
   import sys
   import numpy
   return "succeeded, as expected"
 $$ LANGUAGE plpython3u;
 
-SELECT import_succeed();
+SELECT import_succeed3();
 
-CREATE TYPE named_value AS (
+CREATE TYPE named_value3 AS (
 	  name  text,
 	  value  integer);
 
-CREATE OR REPLACE FUNCTION make_pair_sets (name text)
-RETURNS SETOF named_value AS $$
+CREATE OR REPLACE FUNCTION make_pair_sets3 (name text)
+RETURNS SETOF named_value3 AS $$
   import numpy as np
   return ((name, i) for i in np.arange(1))
 $$ LANGUAGE plpython3u;
 
-SELECT * from make_pair_sets('test');
+SELECT * from make_pair_sets3('test');
