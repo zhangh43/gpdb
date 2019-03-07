312941c99238a066c18d458757ecdb9984d06fb6
Hubert Zhang <hubertzhang@apache.org>
2019-03-07 15:12:26 +0800

    quick fix to gppkg build

--- gpMgmt/bin/gppylib/programs/gppkg.py
+++ gpMgmt/bin/gppylib/programs/gppkg.py
@@ -75,12 +75,12 @@ class GpPkgProgram:
                 raise ExceptionNoStackTraceNeeded('Invalid syntax, expecting "gppkg --migrate <from_gphome> <to_gphome>".')
             self.migrate = (args[0], args[1])
 
-        # gppkg should check gpexpand status
-        check_result, msg = gp.conflict_with_gpexpand("gppkg",
+		if not self.build:
+        	check_result, msg = gp.conflict_with_gpexpand("gppkg",
                                                       refuse_phase1=True,
                                                       refuse_phase2=False)
-        if not check_result:
-            raise ExceptionNoStackTraceNeeded(msg)
+        	if not check_result:
+            	raise ExceptionNoStackTraceNeeded(msg)
 
     @staticmethod
     def create_parser():
